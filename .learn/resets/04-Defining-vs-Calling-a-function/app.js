4d1e2191420a96dbd7275db9b68ccbefa71fcc64
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-08-26 22:42:02 +0000

    realizando tareas de JS

--- .learn/resets/04-Defining-vs-Calling-a-function/app.js
+++ .learn/resets/04-Defining-vs-Calling-a-function/app.js
@@ -1,7 +1,5 @@
 // Define the function called "multi" that expects 2 parameters:
-function multi(numero1, numero2) {
-   return numero1 * numero2; 
-}
+
 
 // Don't edit anything below this line
 let returnValue = multi(7,53812212);
--- exercises/08-Function-parameters/app.js
+++ exercises/08-Function-parameters/app.js
@@ -1,6 +1,7 @@
-// Your code goes here:
-
-
+const renderPerson = (name, birthDay, eyesColor, age, gender) => {
+    return `${name} is a ${age} years old ${gender} born in ${birthDay} with ${eyesColor} eyes`;
+}
 
 ///*** Do not edit below this line ***///
 console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
+
